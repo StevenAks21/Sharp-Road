726a2a2db9774d3d606360fa036adc6a4e8ac9b0
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-11-10 22:16:08 +1000

    s

--- Back-End/main.js
+++ Back-End/main.js
@@ -1,9 +1,12 @@
 const express = require(`express`)
 const app = express()
+require(`dotenv`).config()
 const port = process.env.PORT
 
 app.get('/hi', (req, res) => {
     res.json({ hi: `hi` })
 })
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`listening at port ${port}`)
+})
