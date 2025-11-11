3de84dffc2dfb4888e10dc4114a497463ba665e1
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-11-11 10:21:50 +1000

    Update income.js

--- Back-End/routes/income.js
+++ Back-End/routes/income.js
@@ -2,7 +2,7 @@ const express = require(`express`)
 const router = express.Router()
 
 router.get(`/`, (req, res) => {
-    res.json({ hey: `hey` })
+    res.json({ hey: 1000 })
 })
 
 module.exports = router;
