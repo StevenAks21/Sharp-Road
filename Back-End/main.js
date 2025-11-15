f3ab1a66ab3ca812d4e9eda6469b632e03b4bfa3
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-11-16 01:23:57 +1000

    added sql

--- Back-End/main.js
+++ Back-End/main.js
@@ -2,11 +2,17 @@ const express = require(`express`)
 const app = express()
 require(`dotenv`).config()
 const port = process.env.PORT
+const employees = require("./routes/employees")
+
+app.use(express.json())
+
 
 // Get All Routes
 const income = require(`./routes/income`)
 
-app.use(`/incomes`, income)
+app.use(`/income`, income)
+
+app.use("/employees", employees)
 
 app.listen(port, () => {
     console.log(`listening at port ${port}`)
