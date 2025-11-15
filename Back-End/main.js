d9eff3e3241c0813f0b94dd431a616397099964a
StevenAks21 <stevenalexander0421@gmail.com>
2025-11-15 23:28:59 +1000

    modified:   Back-End/main.js

--- Back-End/main.js
+++ Back-End/main.js
@@ -6,7 +6,7 @@ const port = process.env.PORT
 // Get All Routes
 const income = require(`./routes/income`)
 
-app.use(`/income`, income)
+app.use(`/incomes`, income)
 
 app.listen(port, () => {
     console.log(`listening at port ${port}`)
