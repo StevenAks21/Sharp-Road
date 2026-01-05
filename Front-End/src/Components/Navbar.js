2834b72f65c879e60340171555797e896a625b0f
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2026-01-05 13:02:20 +1000

    Added Navbar

--- Front-End/src/Components/Navbar.js
+++ Front-End/src/Components/Navbar.js
@@ -1,12 +1,50 @@
+import { NavLink } from "react-router-dom";
+import Style from "../Style/Navbar.module.css"
 
+function Navbar() {
+    return (
+        <nav className={Style.navbar}>
+            <h1 className={Style.logo}>SharpRoad</h1>
 
+            <div className={Style.links}>
+                <NavLink
+                    to="/employees"
+                    className={({ isActive }) =>
+                        isActive ? Style.activeLink : Style.link
+                    }
+                >
+                    Employees
+                </NavLink>
 
-function Navbar(){
-    return(
-        <div>
-            <h2>SharpRoad Navbar</h2>
-        </div>
-    )
+                <NavLink
+                    to="/income"
+                    className={({ isActive }) =>
+                        isActive ? Style.activeLink : Style.link
+                    }
+                >
+                    Income
+                </NavLink>
+
+                <NavLink
+                    to="/users"
+                    className={({ isActive }) =>
+                        isActive ? Style.activeLink : Style.link
+                    }
+                >
+                    Users
+                </NavLink>
+
+                <NavLink
+                    to="/inventory"
+                    className={({ isActive }) =>
+                        isActive ? Style.activeLink : Style.link
+                    }
+                >
+                    Inventory
+                </NavLink>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
--- Front-End/src/Pages/Home.js
+++ Front-End/src/Pages/Home.js
@@ -1,10 +1,20 @@
 import Navbar from '../Components/Navbar'
 
+
+async function logToken(){
+    const token = localStorage.getItem('token');
+    const url = `http://localhost:3001/employees/getall`
+    const response = await fetch(url, {headers: {'Authorization': `Bearer ${token}`}});
+    const data = await response.json();
+    console.log("Response Data:", data);
+    console.log("Token from localStorage:", token);
+}
 function Home() {
     return (
         <div>
             <Navbar></Navbar>
             <h1>Home Page</h1>
+            <button onClick={logToken}>Log Token</button>
         </div>
     )
 }
